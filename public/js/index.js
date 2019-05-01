00c01a12e66a7e140438f1bf469be87f116e0fcf
thachd <thach.d@workhouse.me>
2019-05-01 10:27:00 +0700

    Add form message using jquery

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,26 @@ socket.on('disconnect', () => {
 
 socket.on('newMessage', function (message) {
   console.log('New message', message)
+  var li = jQuery('<li></li>')
+  li.text(`${message.from}: ${message.text}`)
+
+  jQuery('#messages').append(li)
+})
+
+// socket.emit('createMessage', {
+//   from: 'Thach',
+//   text: 'Callback'
+// }, function (data) {
+//   console.log('Callback:', data)
+// })
+
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault()
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {
+    
+  })
 })
